f671e24c711b6daa723b18f16256084b4b0f9a3b
apal2702 <apal@twilio.com>
2025-09-02 12:52:47 +0530

    update doc string

--- demo.py
+++ demo.py
@@ -1,4 +1,8 @@
 # Demo Code
+"""
+This module demonstrates basic Python print statements.
+It prints "Hello World" and the result of a simple arithmetic operation.
+"""
 print("Hello World")
 #
 print(1+3)
