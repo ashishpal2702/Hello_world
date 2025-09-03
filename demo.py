b14974d113d74e328ef1a683f70cad8e07dc9c2f
apal2702 <apal@twilio.com>
2025-09-03 09:16:10 +0530

    add area of circle code

--- demo.py
+++ demo.py
@@ -1,8 +1,21 @@
+import sys
+
 # Demo Code
 """
 This module demonstrates basic Python print statements.
 It prints "Hello World" and the result of a simple arithmetic operation.
 """
-print("Hello World")
-#
-print(1+3)
+
+def calculate_area_circle(r):
+    return 3.14 * r * r
+
+def main(r):
+    result = calculate_area_circle(r)
+    print("The area of the circle is:", result)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        radius = float(sys.argv[1])
+    else:
+        radius = 10
+    main(radius)
